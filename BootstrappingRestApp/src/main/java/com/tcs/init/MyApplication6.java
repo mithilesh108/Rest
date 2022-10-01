8243ed8aca7d000c9239a401f578277b8d922f93
Mithilesh Kumar <62215905+mithilesh108@users.noreply.github.com>
2022-10-01 09:26:46 +0530

    Update MyApplication6.java

--- BootstrappingRestApp/src/main/java/com/tcs/init/MyApplication6.java
+++ BootstrappingRestApp/src/main/java/com/tcs/init/MyApplication6.java
@@ -1,38 +1,14 @@
 /*package com.tcs.init;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import com.tcs.resource.StockReport;
-import com.tcs.resource.WeatherReport;
-
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-// #6 option to bootstrapping the rest application  using @ApplicationPath("/api/*") annotations, in this case web.xml not required
+// #6 option to bootstrapping the rest application  using @ApplicationPath("/api/*") annotations,
+//  in this case web.xml not required container will create resource obj as  req per obj model 
+ 
 @ApplicationPath("/api/*")
 public class MyApplication6 extends Application {
 
-	private Set<Class<?>> classes;
-	private Set<Object> singletons;
-
-	public MyApplication6() {	
-		singletons = new HashSet<Object>();
-		classes = new HashSet<Class<?>>();
-		//below obj will follow singleton obj model
-		singletons.add(new StockReport());
-		//below obj will follow request per obj model
-		classes.add(WeatherReport.class);
-	}
-
-	@Override
-	public Set<Object> getSingletons() {
-		return singletons;
-	}
-
-	@Override
-	public Set<Class<?>> getClasses() {
-		return classes;
-	}
 }
+
 */
